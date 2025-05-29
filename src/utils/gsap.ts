52ad03cb997a2f14f5d0325835d2aac560e9921d
Hinako Terakata <terayeah.1224@gmail.com>
2025-05-29 09:23:27 +0900

    feat: gyozaのFlipアニメーション作成

--- src/utils/gsap.ts
+++ src/utils/gsap.ts
@@ -1,8 +1,9 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { SplitText } from "gsap/SplitText";
+import { Flip } from "gsap/Flip";
 
 // 必要なプラグインを登録
-gsap.registerPlugin(DrawSVGPlugin, SplitText);
+gsap.registerPlugin(DrawSVGPlugin, SplitText, Flip);
 
-export { gsap, DrawSVGPlugin, SplitText };
+export { gsap, DrawSVGPlugin, SplitText, Flip };
