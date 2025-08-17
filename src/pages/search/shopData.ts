c3fa58d673ab66614c48da51250d14c27c05b264
Hinako Terakata <terayeah.1224@gmail.com>
2025-08-17 22:41:24 +0900

    feat: checkいれたあとの挙動

---
+++ src/pages/search/shopData.ts
@@ -0,0 +1,22 @@
+import type { ShopData } from "@/types/type";
+
+export const data: ShopData[] = [
+  {
+    img: "",
+    shopName: "餃子",
+    description: "美味しい1",
+    area: "東京",
+  },
+  {
+    img: "",
+    shopName: "",
+    description: "美味しい2",
+    area: "栃木",
+  },
+  {
+    img: "",
+    shopName: "",
+    description: "美味しい3",
+    area: "千葉",
+  },
+];
